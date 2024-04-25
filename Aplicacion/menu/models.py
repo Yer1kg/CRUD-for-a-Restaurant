59503011dcd31cd011f633b3d3324f0cb4cf64f9
Yerik Agustín Gonzáles García <Yerikgg17@gmail.com>
2024-04-25 12:43:27 +0100

    fix delete folders and files

--- Aplicacion/menu/models.py
+++ Aplicacion/menu/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.utils import timezone
-from datetime import timedelta
-
 def default_fecha_caducidad():
     return timezone.now() + timedelta(minutes=1)
 
@@ -9,9 +6,7 @@ class plato(models.Model):
     codigo=models.CharField(primary_key=True,max_length=6)
     nombre=models.CharField(max_length=50)
     precio = models.PositiveBigIntegerField()
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    # Usar la función en lugar del lambda para el valor predeterminado
-    fecha_caducidad = models.DateTimeField(default=default_fecha_caducidad)
+ 
 
 
 
--- plato_cleanup/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlatoCleanupConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'plato_cleanup'
